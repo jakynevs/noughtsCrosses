6d6d1bee422e746d73ff7911e20f56f8061e0b69
jakynevs <johnnevilleis@hotmail.co.uk>
2023-10-06 11:15:46 +0200

    Removed hard code of squares

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ squares, onPlay, fill }) {
+  const boardLength = 3
+  const boardRows = [...Array(boardLength).keys()].map((row) => {
+    const boardSquares = [...Array(boardLength).keys()].map((col) => {
+      const i = boardLength * row + col
+      return (
+        <Square
+        key={i}
+        value={squares[i]}
+        onSquareClick={() => handleClick(i)}
+        ></Square>
+      )
+    })
+    return (
+      <div key={row} className="board-row">{boardSquares}</div>
+    )
+  })
+
+
+
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -30,21 +49,7 @@ function Board({ squares, onPlay, fill }) {
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+        {boardRows}
     </div>
   );
 }
@@ -54,7 +59,7 @@ export default function Game() {
   const [count, setCount] = useState(0);
   const fill = count % 2 === 0 ? "O" : "X";
   const currentSquares = history[count];
-
+  let currentMove = count
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, count + 1), nextSquares];
     setHistory(nextHistory);
@@ -72,7 +77,11 @@ export default function Game() {
       : (description = "Go to start");
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move === currentMove ? (
+          <>You are at move #{move}</>
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
       </li>
     );
   });
